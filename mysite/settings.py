91fb9fd86e696ddfbdbffba9c5506fc54c53e082
E-Design <ehlh5773@gmail.com>
2021-03-14 07:20:16 -0700

    add edesigns

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8%m=_*^!fzr0kxbqk63$woa^%dy(b5y%)=l$3m*qmgojsnon*k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'pythonanywhere.com', 'edesigns.pythonanywhere.com']
 
 
 # Application definition
