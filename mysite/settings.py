cd04b42a2e7dbcec7885e1c593e4ccd741d6038f
E-Design <ehlh5773@gmail.com>
2021-03-25 13:32:28 -0700

    updated requirements.text

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,12 +14,11 @@ from pathlib import Path
 import os
 import environ
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 #get environ varible
 env = environ.Env()
@@ -28,6 +27,14 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
